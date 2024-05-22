21327824505a5c9c85cf56092aa8ee6bc9c0cf94
Mohamed Boutarbouch <mohamedboutarbouch48@gmail.com>
2024-05-22 21:45:20 +0100

    Feat: Event added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +16,14 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'is_admin' => true
+        ]);
+
+        User::factory()->create([
+            'name' => 'Courier',
+            'email' => 'courier@example.com',
         ]);
     }
 }
