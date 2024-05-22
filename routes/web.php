29d59f835edbb3157e718022a4f6c5f3fe01446d
Mohamed Boutarbouch <mohamedboutarbouch48@gmail.com>
2024-05-22 22:14:59 +0100

    Fix: Fix courier route

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::get('/courier', function () {
+        return inertia('Courier');
+    })->name('courier');
+
     Route::post('/courier', function (Request $request) {
         $coordinates = $request->all();
 
