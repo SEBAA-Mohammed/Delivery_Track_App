d123d3a25ecbbc994a1db5231f726002bf4d477e
Mohamed Boutarbouch <mohamedboutarbouch48@gmail.com>
2024-05-24 18:47:27 +0100

    Fix: Protect the dashboard route to be seen only by admin users

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 use Inertia\Response;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Controllers\Controller;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -33,7 +33,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(auth()->user()->is_admin ? route('dashboard', absolute: false) : route('courier', absolute: false));
     }
 
     /**
--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -22,20 +22,22 @@ export default function Authenticated({ user, header, children }) {
               </div>
 
               <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                <NavLink
-                  href={route("dashboard")}
-                  active={route().current("dashboard")}
-                >
-                  Dashboard
-                </NavLink>
-
                 {user.is_admin ? (
-                  <NavLink
-                    href={route("admin")}
-                    active={route().current("admin")}
-                  >
-                    Admin
-                  </NavLink>
+                  <>
+                    <NavLink
+                      href={route("dashboard")}
+                      active={route().current("dashboard")}
+                    >
+                      Dashboard
+                    </NavLink>
+
+                    <NavLink
+                      href={route("admin")}
+                      active={route().current("admin")}
+                    >
+                      Admin
+                    </NavLink>
+                  </>
                 ) : (
                   <NavLink
                     href={route("courier")}
